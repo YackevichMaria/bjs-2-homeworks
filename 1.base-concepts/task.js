2633bf3b264ab5bd9088f29dd3bbcbfeff87bc22
Maria <yackevichmaria@gmail.com>
2023-01-09 22:06:01 +0300

    task completed

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,23 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
-  return arr;
+  let D = Math.pow(b, 2) - 4 * a * c;
+  let rootOne;
+  let rootNext;
+
+  if ( D < 0 ){
+    console.log("Дискриминант меньше нуля, корней нет")
+  } else if ( D === 0 ) {
+    rootOne = - b / (2 * a);
+    arr.push( rootOne );
+  } else {
+    rootOne = ( - b + Math.sqrt(D) ) / ( 2 * a );
+    arr.push( rootOne );
+    rootNext = ( - b - Math.sqrt(D) ) / ( 2 * a );
+    arr.push( rootNext );
+  }
+
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
