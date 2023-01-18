b4118101083c5872f5cfeddcf0c39f369715e9f1
Maria <yackevichmaria@gmail.com>
2023-01-18 16:58:17 +0300

    task completed

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,29 @@
-function compareArrays(arr1, arr2) {
-  
-}
+function compareArrays(arr1, arr2) { 
+    return arr1.length == arr2.length && arr1.every((item, index) => item === arr2[index]);
+  }
+
+
 
 function getUsersNamesInAgeRange(users, gender) {
   
+  return users
+    .filter(user => user.gender === gender)
+    .map(person => person.age)
+    .reduce((summ, userAge, index, array) => summ + userAge / array.length, 0)
 }
+
+/* почему не работает код ?
+
+function getUsersNamesInAgeRange(users, gender) {
+  
+  return users
+    .filter(user => user.gender === gender)
+    .map(person => person.age)
+    .reduce((summ, userAge, index, array) => {
+      if (index === array.lenght - 1) {
+        return summ / array.lenght; 
+      }
+
+      return summ + userAge;
+    }, 0)
+} */
