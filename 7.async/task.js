30e32c6467d5ee49c155830ed11db419c0ab361b
Maria <yackevichmaria@gmail.com>
2023-02-04 21:42:33 +0300

    task completed

---
+++ 7.async/task.js
@@ -0,0 +1,60 @@
+"use strict"
+class AlarmClock {
+    constructor() {
+        this.alarmCollection = [];
+        this.intervalId = null;
+    }
+
+    addClock(time, callback) {
+        if (!time || !callback) {
+            throw new Error("Отсутствуют обязательные аргументы");
+        }
+
+        /* let isValid = /^([0-1]?[0-9]|2[0-4]):([0-5][0-9])(:[0-5][0-9])?$/.test(time.value);
+        if (isValid === false) {
+            throw new Error("Ошибка в заданном времени");
+        }*/
+
+        if (this.alarmCollection.some(alarm => alarm.time === time)) {
+            console.warn('Уже присутствует звонок на это же время');
+        }
+
+        this.alarmCollection.push({time, callback, canCall: true});
+    }
+
+    removeClock(time) {
+        this.alarmCollection = this.alarmCollection.filter(alarm => alarm.time !== time);
+    }
+
+    getCurrentFormattedTime() {
+        return new Date().toTimeString().slice(0, 5);
+        /*return new Data().toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });*/
+    }
+
+    start() {
+        if (this.intervalId) {
+            return;
+        }
+
+        this.intervalId = setInterval(() => this.alarmCollection.forEach(alarm => {
+            if (alarm.time === this.getCurrentFormattedTime() && alarm.canCall) {
+                alarm.canCall = false;
+                alarm.callback();
+            }
+        }), 1000);
+    }
+
+    stop() {
+        clearInterval(this.intervalId);
+        this.intervalId = null;
+    }
+
+    resetAllCalls() {
+        this.alarmCollection.forEach(alarm => alarm.canCall = true);
+    }
+
+    clearAlarms() {
+        this.stop();
+        this.alarmCollection = [];
+    }
+}
